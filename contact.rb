9bf6445b0b8624d064f80c8222402c550d714e94
Lena <fatmeow@gmail.com>
2017-11-08 21:54:11 -0500

    Complete Contact class

--- contact.rb
+++ contact.rb
@@ -1,32 +1,122 @@
 class Contact
+  @@contacts = []
+  @@id = 1
 
   # This method should initialize the contact's attributes
-  def initialize
-
+  def initialize(first_name, last_name, email, note = "N/A")
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+    @id = @@id
+    @@id += 1
   end
 
-  # This method should call the initializer, 
+  # This method should call the initializer,
   # store the newly created contact, and then return it
-  def self.create
-
+  def self.create(first_name, last_name, email, note = "N/A")
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
   end
 
   # This method should return all of the existing contacts
   def self.all
-
+    return @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(by_id)
+    result = nil
+    @@contacts.each do |contact|
+      if contact.id == by_id
+        result = contact
+      end
+    end
+    return result
   end
 
-  # This method should allow you to specify 
+  # a getter (*get* the value of an attribute)
+  def email
+    @email
+  end
+
+  def id
+    @id
+  end
+
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def note
+    @note
+  end
+
+  # a setter (*set* the value of an attribute)
+  def note=(note)
+    @note = note
+  end
+
+  def first_name=(name)
+    @first_name = name
+  end
+
+  def last_name=(name)
+    @last_name = name
+  end
+
+  def email=(email)
+    @email = email
+  end
+
+  # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
+  def update(attribute = nil, value = nil)
+    if attribute == nil && value == nil
+      puts "Do you wish to update 'first name', 'last name', 'email', or 'note' for #{full_name}?"
+      attribute = gets.chomp
+      # check for valid user input
+      until attribute == "first name" || attribute == "last name" || attribute == "email" || attribute == "note"
+        puts "Invalid entry. Type 'first name', 'last name', 'email', or 'note'."
+        attribute = gets.chomp
+      end
+
+      if attribute == "first name"
+        puts "Enter new first name:"
+        value = gets.chomp
+        first_name = value
+      elsif attribute == "last name"
+        puts "Enter new last name:"
+        value = gets.chomp
+        last_name = valu
+      elsif attribute == "email"
+        puts "Enter new e-mail:"
+        value = gets.chomp
+        email = value
+      elsif attribute == "note"
+        puts "Enter new note:"
+        value = gets.chomp
+        note = value
+      end
+    end
+
+    if attribute == "first name"
+      self.first_name = value
+    elsif attribute == "last name"
+      self.last_name = value
+    elsif attribute == "email"
+      self.email = value
+    elsif attribute == "note"
+      self.note = value
+    end
 
   end
 
@@ -34,25 +124,57 @@ class Contact
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
+  def self.find_by(attribute, value)
+    if attribute == "first name"
+      Contact.all.each do |c|
+        if c.first_name == value
+          return c
+        end
+      end
+      puts "No results found"
+      return false
+    elsif attribute == "last name"
+      Contact.all.each do |c|
+        if c.last_name == value
+          return c
+        end
+      end
+      puts "No results found"
+      return false
+    elsif attribute == "email"
+      Contact.all.each do |c|
+        if c.email == value
+          return c
+        end
+      end
+      puts "No results found"
+      return false
+    elsif attribute == "note"
+      Contact.all.each do |c|
+        if c.note.include? value
+          return c
+        end
+      end
+      puts "No results found"
+      return false
+    end
   end
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    @@contacts = []
   end
 
   def full_name
-
+    return "#{@first_name} #{@last_name}"
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    @@contacts.delete(self)
   end
 
   # Feel free to add other methods here, if you need them.
-  
+
 end
